419ce8f7d23f359db63f0862ffa11e0053a4661e
Silviu Caragea <silviu.caragea@woowteam.com>
2016-05-11 21:46:56 +0300

    Remove unused code

--- src/beanstalkd_consumer.erl
+++ src/beanstalkd_consumer.erl
@@ -10,9 +10,6 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE).
--define(DEFAULT_WAIT_TIME, 2000).
-
 -record(state, {
     connection,
     connection_state,
--- src/beanstalkd_queue.erl
+++ src/beanstalkd_queue.erl
@@ -11,8 +11,6 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE).
-
 -record(state, {job_queue, connection, connection_state}).
 
 start_link(Args) ->
